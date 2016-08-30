f47011c446169d38244eb61ea02feb83a33027a5
Jonathan Ebert <jebert142@gmail.com>
2016-08-30 01:25:22 -0400

    Drink model added, misc changes

--- Bartender/Controllers/MenuController.cs
+++ Bartender/Controllers/MenuController.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bartender.Views.Menu;
 
 namespace Bartender.Controllers
 {
+
+    
     public class MenuController : Controller
     {
         // GET: Menu
+        //View has a red line under itself?
         public ActionResult Index()
         {
             return View();
         }
     }
-
 
 }
---
+++ Bartender/Views/Menu/Drink.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bartender.Views.Menu
+{
+    //Drink model includes drink name, description, alcohol content, and price.
+    public class Drink
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double AlcoholContent { get; set; }
+        public double Price { get; set; }
+    }
+}
