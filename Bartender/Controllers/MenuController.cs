3937bc522468d29ac8e19796469527bd4a219271
Jonathan Ebert <jebert142@gmail.com>
2016-08-30 09:02:16 -0400

    OrderController added. Scattered comments added

--- Bartender/Controllers/MenuController.cs
+++ Bartender/Controllers/MenuController.cs
@@ -3,21 +3,25 @@
 namespace Bartender.Controllers
 {
 
-
+    //MenuController contains all the methods which return the views
     public class MenuController : Controller
     {
         // GET: Menu
-        //View has a red line under itself?
+        //Views have a red line under them?
+
+        //returns the Index.cshtml view
         public ActionResult Index()
         {
             return View();
         }
 
+        //returns the Order.cshtml view
         public ActionResult Orders()
         {
             return View();
         }
 
+        //returns the Prepared.cshtml view
         public ActionResult Prepared()
         {
             return View();
---
+++ Bartender/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bartender.Controllers
+{
+    //This controller will house each method for each step in the order process
+    public class OrderController : Controller
+    {
+        //Default controller method commented out, leaving it here for now
+        // GET: Order
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
