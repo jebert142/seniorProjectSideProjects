5f630842124f8388ca7382e3bd71d7e05e52bfde
Jonathan Ebert <jebert142@gmail.com>
2016-09-02 21:25:58 -0400

    tinkered, no shown progress but I have a better idea for what I'm doing next

--- Bartender/Controllers/OrderController.cs
+++ Bartender/Controllers/OrderController.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bartender.Models;
 
 namespace Bartender.Controllers
 {
-    //This controller will house each method for each step in the order process and other necessary objects
+    //This controller houses each method for each step in the order process and other necessary objects
+    //Those methods need to add...
     public class OrderController : Controller
     {
-        //Default controller method commented out, leaving it here for now
-        // GET: Order
-        //public ActionResult Index()
+        //public ActionResult Orders()
         //{
         //    return View();
         //}
